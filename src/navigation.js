d07c1a191457a9de7a6e4136ca1dcba1d13c8514
Lizzie Wilkinson <57641674+lizziefen1@users.noreply.github.com>
2023-08-12 16:29:30 -0500

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -81,32 +81,21 @@ export const footerData = {
         { text: 'Status', href: '#' },
       ],
     },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+
   ],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+
+    //{ ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    //{ ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/lizziefen1' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+   
+    Made by Lizzie</a> · All rights reserved.
   `,
 };
